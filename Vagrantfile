56391929981d392127da5db20939cb2776d31c50
edersonbrilhante <ederson.brilhante@corp.globo.com>
2017-01-12 18:15:04 -0200

    Changed Vagrantfile

--- Vagrantfile
+++ Vagrantfile
@@ -2,7 +2,7 @@ VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box = "ubuntu/trusty64"
-  config.vm.network "private_network", ip: "10.0.0.4", auto_config: "false"
+  config.vm.network "private_network", ip: "192.168.33.10", auto_config: "false"
   config.vm.hostname = "ROUTESSERVICE"
   config.omnibus.chef_version = :latest
   config.vm.provider "virtualbox" do |v|
--- vagrant_provision.sh
+++ vagrant_provision.sh
@@ -3,16 +3,26 @@
 apt-get update
 apt-get install git -y
 
+# Download GO
+cd /tmp/
 wget https://storage.googleapis.com/golang/go1.7.4.linux-amd64.tar.gz
 
-tar -C /usr/local -xzf go$VERSION.$OS-$ARCH.tar.gz
+# Extracts Files
+tar -C /usr/local -xzf go1.7.4.linux-amd64.tar.gz
 
+# Exports PATH 
 export PATH=$PATH:/usr/local/go/bin
 
-mkdir $HOME/work
-export GOPATH=$HOME/work
+# Exports GOPATH
+export GOPATH=$HOME
 
-cd work
-
+# Install gobgp
+cd /vagrant
 go get github.com/osrg/gobgp/gobgpd
 go get github.com/osrg/gobgp/gobgp
+
+#Install echo
+go get github.com/labstack/echo
+
+#Start gobgpd and webserver
+$HOME/bin/gobgpd & go run main.go & 
